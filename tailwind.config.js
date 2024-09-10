a28589447c9038cc0ee39064dd2fb1105676f346
JimMono69 <jimkaiolakala@gmail.com>
2024-09-10 11:06:22 +0300

    Starting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    darkMode: ["class"],
+    content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx,}',
+	'./src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	container: {
+		center: true,
+		padding: "15px",
+	},
+	screens: {
+		sm: "640px",
+		md: "768px",
+		lg: "960px",
+		xl: "1200px",
+		},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	},
+  	extend: {
+		colors: {
+			primary: "#1c1c22",
+			accent: {
+				DEFAULT: "#00ff99",
+				hover: "#00e187",
+			},
+  		},
+		keyframes: {
+			"accordion-down": {
+				from: { height: "0" },
+				to: { height: "var(--radix-accordion-content-height)" },
+			},
+			"accordion-up": {
+				from: { height: "var(--radix-accordion-content-height)" },
+				to: { height: "0" },
+				},
+			},
+		animation: {
+			"accordion-down": "accordion-down 0.2s ease-out",
+			"accordion-up": "accordion-up 0.2s ease-out"
+		},
+  	},
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
