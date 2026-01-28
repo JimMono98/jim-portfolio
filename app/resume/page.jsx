2e53c37824631f6c87cda19372868659e0cd6937
JimMono98 <jimkaiolakala@gmail.com>
2026-01-29 01:22:28 +0200

    update experience details and email address in resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -19,7 +19,7 @@ const about = {
         },
         {
             fieldName: "Experience",
-            fieldValue: "8+ Years"
+            fieldValue: `${calculateYearsExperience()}+ Years`
         },
         {
             fieldName: "LinkedIn",
@@ -31,11 +31,11 @@ const about = {
         },
         {
             fieldName: "Email",
-            fieldValue: "jimkaiolakala@gmail.com"
+            fieldValue: "dmonogenidis@gmail.com"
         },
         {
-            fieldName: "Freelance",
-            fieldValue: "Available"
+            fieldName: "Working Status",
+            fieldValue: "Delloite - Full Stack Software Engineer"
         },
         {
             fieldName: "Languages",
@@ -50,6 +50,11 @@ const experience = {
     title: "My experience",
     description: "I have a diverse background, combining hands-on experience in hospitality with a strong foundation in web development and software engineering. While working as a Human Resources Manager and Web Developer at Hotel Paradise Kriopigi, I enhanced both technical systems and team management. My time in hospitality roles at NAMMOS Mykonos and Villas Seaside Lounge has strengthened my adaptability and multitasking abilities.",
         items: [
+        {
+            company: "Delloite",
+            position: "Full Stack Software Engineer",
+            duration: "2024 - Present",
+        },
         {
             company: "Hotel Paradise Kriopigi",
             position: "Human Resources Manager",
@@ -234,12 +239,17 @@ const skills = {
     ],
 };
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
+
+const calculateYearsExperience = () => {
+    const startYear = 2018;
+    const currentYear = new Date().getFullYear();
+    return currentYear - startYear;
+};
 
 const Resume = () => {
     return (
