81baef9c794d35b7de878742b3cccb53ce73993a
JimMono98 <jimkaiolakala@gmail.com>
2026-02-01 01:54:41 +0200

    feat: add new Work section with project details

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
-    "./app/**/*.{js,jsx,}",
-    "./src/**/*.{js,jsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
