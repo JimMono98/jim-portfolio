47c8d735be0dfd527d7ae56c0e5cda439f1d3a72
JimMono98 <jimkaiolakala@gmail.com>
2026-02-25 01:04:54 +0200

    cdn - false

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -4,5 +4,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false,
 });
